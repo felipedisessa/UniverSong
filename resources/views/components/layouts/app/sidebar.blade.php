cae943f4575be512a367cfc186863fb1d88bf515
Felipe Di Sessa <felipetmds@hotmail.com>
2025-06-06 16:30:13 -0300

    Wip: trocado logo para testes

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -13,8 +13,8 @@
 
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Platform')" class="grid">
-                    <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
-                    <flux:navlist.item icon="musical-note" :href="route('songs.index')" :current="request()->routeIs('songs.index')" wire:navigate>
+                    <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Pagina Inicial') }}</flux:navlist.item>
+                    <flux:navlist.item icon="musical-note" :href="route('songs.index')" :current="request()->routeIs('songs.index')">
                         {{ __('Minhas Letras') }}
                     </flux:navlist.item>
 
@@ -23,15 +23,15 @@
 
             <flux:spacer />
 
-            <flux:navlist variant="outline">
-                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                {{ __('Repository') }}
-                </flux:navlist.item>
+{{--            <flux:navlist variant="outline">--}}
+{{--                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">--}}
+{{--                {{ __('Repository') }}--}}
+{{--                </flux:navlist.item>--}}
 
-                <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
-                {{ __('Documentation') }}
-                </flux:navlist.item>
-            </flux:navlist>
+{{--                <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">--}}
+{{--                {{ __('Documentation') }}--}}
+{{--                </flux:navlist.item>--}}
+{{--            </flux:navlist>--}}
 
             <!-- Desktop User Menu -->
             <flux:dropdown class="hidden lg:block" position="bottom" align="start">
